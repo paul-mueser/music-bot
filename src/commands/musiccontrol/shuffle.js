af07a5b33da1483a423e328bbd6e3ce5a47df18f
Paul Müser <github@paulmueser.de>
2024-03-24 21:51:45 +0100

    add shuffle and toggleloop commands

---
+++ src/commands/musiccontrol/shuffle.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+    callback: async (client, interaction) => {
+        await interaction.deferReply();
+
+        const queue = client.player.nodes.get(interaction.guild);
+
+        if (!queue) {
+            return interaction.editReply({content: 'There is no song playing.', ephemeral: true});
+        }
+
+        queue.tracks.shuffle();
+
+        await interaction.reply('I\'ve mixed things up a bit in the queue.');
+    },
+
+    name: 'shuffle',
+    description: 'Shuffles the queue.',
+};
