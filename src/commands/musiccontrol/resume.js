caf5c7a4cb4ab30f50e85f02e7cb4c1f17796d7e
Paul Müser <github@paulmueser.de>
2024-03-24 22:01:59 +0100

    add button updates for pause and resume command

--- src/commands/musiccontrol/resume.js
+++ src/commands/musiccontrol/resume.js
@@ -1,3 +1,5 @@
+const {ButtonBuilder, ButtonStyle, ActionRowBuilder} = require("discord.js");
+const {button} = require("../../utils/constants");
 module.exports = {
 
     callback: async (client, interaction) => {
@@ -12,6 +14,22 @@ module.exports = {
         queue.node.resume();
 
         await interaction.editReply('Resume playing.')
+
+        if (queue.dashboard) {
+            if (queue.dashboard) {
+                let playing = queue.node.isPaused();
+                let loop = queue.repeatMode === 1;
+
+                const playPauseButton = new ButtonBuilder().setCustomId('Playing-PlayPause').setEmoji(playing ? button.play : button.pause).setStyle(playing ? ButtonStyle.Success : ButtonStyle.Secondary);
+                const skipButton = new ButtonBuilder().setCustomId('Playing-Skip').setEmoji(button.skip).setStyle(ButtonStyle.Secondary);
+                const stopButton = new ButtonBuilder().setCustomId('Playing-Stop').setEmoji(button.stop).setStyle(ButtonStyle.Danger);
+                const loopButton = new ButtonBuilder().setCustomId('Playing-Loop').setEmoji(button.loop).setStyle(loop ? ButtonStyle.Danger : ButtonStyle.Secondary);
+                const shuffleButton = new ButtonBuilder().setCustomId('Playing-Shuffle').setEmoji(button.shuffle).setStyle(ButtonStyle.Secondary);
+                const row = new ActionRowBuilder().addComponents(playPauseButton, skipButton, stopButton, loopButton, shuffleButton);
+
+                queue.dashboard.edit({ components: [row] });
+            }
+        }
     },
 
     name: 'resume',
