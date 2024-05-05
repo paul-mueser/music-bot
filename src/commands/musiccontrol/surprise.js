93b5e5774114783d4ac3fb536be5226e819d370b
Paul Müser <kontakt@paulmueser.de>
2024-05-05 22:55:42 +0200

    update surprise

--- src/commands/musiccontrol/surprise.js
+++ src/commands/musiccontrol/surprise.js
@@ -1,20 +1,25 @@
 const oneSongLoopHandler = require('../../handlers/oneSongLoopHandler')
 const {wait} = require("../../utils/wait");
 const editQueueDashboard = require("../../utils/editQueueDashboard");
+const {QueryType} = require("discord-player");
+const {ApplicationCommandOptionType} = require('discord.js');
 
 module.exports = {
 
     callback: async (client, interaction) => {
+        const count = interaction.options.get('count')?.value || 0;
+
         const songList = [
-            'https://www.youtube.com/watch?v=TaV1r341wYk',
-            'https://www.youtube.com/watch?v=hLeUL4i9PNw',
-            'https://www.youtube.com/watch?v=jgw0SAj9UXg',
-            'https://www.youtube.com/watch?v=JY0UKypROAw',
-            'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-            'https://www.youtube.com/watch?v=0-wVealwtsg',
-            'https://www.youtube.com/watch?v=4G6QDNC4jPs',
-            'https://www.youtube.com/watch?v=TOb2NXx_GQo',
-            'https://www.youtube.com/watch?v=uiYQ3SPIkLQ',
+            'https://www.youtube.com/watch?v=TaV1r341wYk', // Ghostbusters Theme Song
+            'https://www.youtube.com/watch?v=hLeUL4i9PNw', // The Ketchup Song
+            'https://www.youtube.com/watch?v=jgw0SAj9UXg', // Lillifee Dance
+            'https://www.youtube.com/watch?v=JY0UKypROAw', // Who's that wonderful girl
+            'https://www.youtube.com/watch?v=dQw4w9WgXcQ', // Rick Astley - Never Gonna Give You Up
+            'https://www.youtube.com/watch?v=0-wVealwtsg', // Ich nehm dich bei der Hand
+            'https://www.youtube.com/watch?v=4G6QDNC4jPs', // Cascada - Everytime We Touch
+            'https://www.youtube.com/watch?v=TOb2NXx_GQo', // Mädchen auf dem Pferd
+            'https://www.youtube.com/watch?v=uiYQ3SPIkLQ', // Kreisligalegende - Mannschaft
+            'https://www.youtube.com/watch?v=hVVxPzinj9Y', // Metronomi schmomi
         ]
 
         const songLink = songList[Math.floor(Math.random() * songList.length)];
@@ -25,6 +30,21 @@ module.exports = {
         const queue = client.player.nodes.get(interaction.guild);
         queue.setRepeatMode(0);
 
+        for (let i = 1; i < count; i++) {
+            const songLink = songList[Math.floor(Math.random() * songList.length)];
+            const result = await client.player.search(songLink, {
+                requestedBy: interaction.usage,
+                searchEngine: QueryType.AUTO,
+            });
+
+            if (!result.hasTracks() || result.isEmpty()) {
+                await interaction.editReply({content: 'No results found!', ephemeral: true});
+                return;
+            }
+
+            await queue.insertTrack(result.tracks[0], 0);
+        }
+
         if (queue.dashboard) {
             const row = editQueueDashboard(queue);
 
@@ -34,4 +54,11 @@ module.exports = {
 
     name: 'surprise',
     description: 'Surprises you.',
+    options: [
+        {
+            name: 'count',
+            description: 'How many times do you want to be surprised?',
+            type: ApplicationCommandOptionType.Integer,
+        },
+    ],
 };
