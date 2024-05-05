540078cd9c14807c41c4060632dc2b1f914971b4
Paul Müser <kontakt@paulmueser.de>
2024-05-05 23:22:44 +0200

    update scar command

--- src/commands/musiccontrol/scar.js
+++ src/commands/musiccontrol/scar.js
@@ -1,11 +1,16 @@
 const oneSongLoopHandler = require('../../handlers/oneSongLoopHandler')
+const {wait} = require("../../utils/wait");
 
 module.exports = {
 
     callback: async (client, interaction) => {
         const songLink = 'https://www.youtube.com/watch?v=0N2TmZVBLwM';
         const replyText = 'Warum liebt mich niemand? 🦁';
-        await oneSongLoopHandler(client, interaction, songLink, replyText)
+        await oneSongLoopHandler(client, interaction, songLink, replyText);
+
+        await wait(500);
+        const queue = client.player.nodes.get(interaction.guild);
+        queue.setRepeatMode(0);
     },
 
     name: 'scar',
